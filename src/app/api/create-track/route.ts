ca76e1fd22b3ec00c6add732613d0e383319c24c
Snow Yang <snow.yang@fluv.com>
2023-12-07 14:31:10 +0900

    Add route for creating a track table

---
+++ src/app/api/create-track/route.ts
@@ -0,0 +1,18 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  try {
+    const result = await sql`
+      CREATE TABLE track (
+        id SERIAL PRIMARY KEY,
+        task_name varchar(255) NOT NULL,
+        days_to_accomplish int NOT NULL,
+        task_completion_time timestamp
+      );
+    `;
+    return NextResponse.json({ result }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
